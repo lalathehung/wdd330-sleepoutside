8ec4fc9f80c7bd2a9b91b58c185719b1f655f5ce
lalathehung <1808arthur@gmail.com>
2025-05-31 17:36:43 -0300

    Updated

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-
+import ProductDetails from "./ProductDetails.mjs";
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
+
+const product = new ProductDetails(productID, dataSource);
+product.init();
+
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
